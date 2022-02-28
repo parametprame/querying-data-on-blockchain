d8bde35b663acf60cf642724cd9564faea589e53
Paramet Kongjaroen <43013637+parametprame@users.noreply.github.com>
2022-02-28 13:39:38 +0700

    add schema for qurying marketplace ladsdle

--- src/mapping.ts
+++ src/mapping.ts
@@ -6,60 +6,62 @@ import {
   Offered,
   Sale
 } from "../generated/MarketPlace/MarketPlace"
-import { ExampleEntity } from "../generated/schema"
+import { ListingEntity, SaleEntity, HistoryNFt, OfferEntity} from "../generated/schema"
 
-export function handleCancel(event: Cancel): void {
-  // Entities can be loaded from the store using a string ID; this ID
-  // needs to be unique across all entities of the same type
-  let entity = ExampleEntity.load(event.transaction.from.toHex())
+export function handleCancel(event: Cancel): void {}
 
-  // Entities only exist after they have been saved to the store;
-  // `null` checks allow to create entities on demand
-  if (!entity) {
-    entity = new ExampleEntity(event.transaction.from.toHex())
+export function handleListed(event: Listed): void {
+  let listing = ListingEntity.load(event.params.listingId.toString())
 
-    // Entity fields can be set using simple assignments
-    entity.count = BigInt.fromI32(0)
+  if(!listing) {
+    listing = new ListingEntity(event.params.listingId.toString())
+    listing.price = event.params.price
+    listing.listingId = event.params.listingId
+    listing.seller = event.params.seller
+    listing.token = event.params.token
+    listing.ListingStatus = event.params.status
+    listing.tokenId = event.params.tokenId
+    listing.owner = event.params.seller.toHexString()
   }
 
-  // BigInt and BigDecimal math are supported
-  entity.count = entity.count + BigInt.fromI32(1)
+  listing.save()
 
-  // Entity fields can be set based on event parameters
-  entity.listingId = event.params.listingId
-  entity.seller = event.params.seller
-
-  // Entities can be written to the store with `.save()`
-  entity.save()
-
-  // Note: If a handler doesn't require existing field values, it is faster
-  // _not_ to load the entity from the store. Instead, create it fresh with
-  // `new Entity(...)`, set the fields that should be updated and save the
-  // entity back to the store. Fields that were not set or unset remain
-  // unchanged, allowing for partial updates to be applied.
-
-  // It is also possible to access smart contracts from mappings. For
-  // example, the contract that has emitted the event can be connected to
-  // with:
-  //
-  // let contract = Contract.bind(event.address)
-  //
-  // The following functions can then be called on this contract to access
-  // state variables and other data:
-  //
-  // - contract._listingId(...)
-  // - contract._listings(...)
-  // - contract._offers(...)
-  // - contract.countListing(...)
-  // - contract.countingOffer(...)
-  // - contract.getListing(...)
-  // - contract.getListings(...)
-  // - contract.offeringId(...)
-  // - contract.showOfferforListitem(...)
 }
 
-export function handleListed(event: Listed): void {}
+export function handleOffered(event: Offered): void {
+  let offer = OfferEntity.load(event.params.tokenId.toString())
 
-export function handleOffered(event: Offered): void {}
+  if(!offer) {
+    offer = new OfferEntity(event.params.tokenId.toString())
+    offer.offerer =  event.params.offerer
+    offer.price = event.params.price
+    offer.tokenId = event.params.tokenId
+  }
+  
+  offer.save()
+}
 
-export function handleSale(event: Sale): void {}
+export function handleSale(event: Sale): void {
+  let saleItem = SaleEntity.load(event.params.listingId.toString())
+
+  if(!saleItem) {
+    saleItem = new SaleEntity(event.params.listingId.toString())
+    saleItem.buyer = event.params.buyer
+    saleItem.seller = event.params.seller
+    saleItem.token = event.params.token  
+    saleItem.tokenId = event.params.tokenId
+    saleItem.price = event.params.price
+    saleItem.ListingStatus = event.params.status
+    saleItem.user = event.params.buyer.toHexString()
+    saleItem.nft = event.params.tokenId.toHexString()
+  }
+
+  saleItem.save()
+
+  let nft = HistoryNFt.load(event.params.tokenId.toString())
+
+  if (!nft) {
+    nft = new HistoryNFt(event.params.tokenId.toHexString())
+    nft.save()
+  }
+}
